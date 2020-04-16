a6f7e72b12e7d3f3ebc2d5eed3a6f838bd9d40cf
Dan-Oku <oku_dan411@yahoo.co.jp>
2020-04-16 20:18:19 +0900

    serialのクラス化、stm32ハンドラの追加

--- SerialHandler.h
+++ SerialHandler.h
@@ -9,34 +9,100 @@
 
 */
 
+class SerialPort
+{
+    private:
+        bool initialized = false;
+        void* handler;
+    public:
+        bool Initialize();
+        bool Initialize(int baudrate);
+        bool Initialize(int baudrate,int num);  //複数ポートがあるマイコン用
+        void WriteByte(uint8_t data);
+        int ReadByte();
+}
+
 #define ARDUINO	//ここにマイコンを追記していく
 //#define MBED
+//#define STM32
 
 #ifdef ARDUINO						//arduinoのSerial用
-//とりあえずやっつけで適当に書いたので後世の人頼んだ
-//いろいろ便利機能つけすぎて再利用性落とさんようにな～^^
 
-bool initialized = false;
-
-void SerilInitialize(long baudrate){
-    if(!initialized){
-        Serial.begin(baudrate);
-        initialized = true;
+bool SerialPort::Initialize(int baudrate,int num){
+    if(num == 0){
+        if(!initialized){
+            Serial.begin(baudrate);
+            initialized = true;
+        }
+        return true;
+    }else{
+        return false;
     }
 }
 
-void SerilInitialize(){
-    SerilInitialize(115200);
+bool SerialPort::Initialize(int baudrate){
+    return Initialize(baudrate,0);
 }
 
-void SerialWriteByte(uint8_t data)
+bool SerialPort::Initialize(){
+    return Initialize(115200,0);
+}
+
+void SerialPort::WriteByte(uint8_t data)
 {
     Serial.write(data);
 }
 
-int SerialReadByte()
+int SerialPort::ReadByte()
 {
     return Serial.read();
+}
+
+#endif
+
+#ifdef STM32						//STM32のSerial用
+
+bool SerialPort::Initialize(int baudrate,int num){
+    if(!initialized){
+        switch (num)
+        {
+        case 1:
+            handler = &huart1;
+            break;
+        case 2:
+            handler = &huart2;
+            break;
+        case 3:
+            handler = &huart3;
+            break;
+        default:
+            break;
+        }
+        (*handler).Init.BaudRate = baudrate;
+        if (HAL_UART_Init(handler) != HAL_OK)return false;
+        initialized = true;
+    }
+    return true;
+}
+
+bool SerialPort::Initialize(int baudrate){
+    return Initialize(baudrate,1);
+}
+
+bool SerialPort::Initialize(){
+    return Initialize(115200,1);
+}
+
+void SerialPort::WriteByte(uint8_t data)
+{
+    HAL_UART_Transmit(handler,&data, 1, 0xF);
+}
+
+int SerialPort::ReadByte()
+{
+    uint8_t data;
+    HAL_UART_Receive(handler,&data, 1, 0xF);
+    return (int)data;
 }
 
 #endif
